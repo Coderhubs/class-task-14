3d201a7ce683d458520cb94169cd8469df425e4c
Sumra Jabeen <160333925+Coderhubs@users.noreply.github.com>
2025-01-04 13:54:20 +0500

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  images: {
+    domains: ['i.pravatar.cc'], // Allow loading images from pravatar.cc
+  },
 };
+
 
 export default nextConfig;
